1474c3a586bd8a1d2a7ead4eca3b070cef347c68
Hugo Avendaño <hugoavendano2014@gmail.com>
2022-04-07 23:28:18 -0300

    Use own dependency

--- src/main/java/com/rooftop/havendan/App.java
+++ src/main/java/com/rooftop/havendan/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(RooftopUtils.multiply(8, 3));
+        System.out.println(RooftopUtils.rest(24, 12));
+        System.out.println(RooftopUtils.sum(10, 17));
     }
 }
