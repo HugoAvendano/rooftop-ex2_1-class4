2d95e7fe0d6496db3d31b39c76bab56a0a51f93b
Hugo Avendaño <hugoavendano2014@gmail.com>
2022-04-07 22:11:51 -0300

    Initial commit

---
+++ src/test/java/com/rooftop/havendan/AppTest.java
@@ -0,0 +1,20 @@
+package com.rooftop.havendan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
